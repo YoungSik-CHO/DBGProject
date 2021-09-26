8f16f6f13580fcb37a6c745a3d1e45f1587d0b5b
SongKyu <zxz3410@gmail.com>
2021-09-26 15:46:36 +0900

    django_kyu

--- DBG/kyusung/views.py
+++ DBG/kyusung/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello Kyusung!")
+
+def test(request):
+    return HttpResponse("Hi test01")
